64f342ffc197fe0ee3ec9c94d0f1b9bf81876f36
martinchoC <martin.e.castro.99@gmail.com>
2018-07-24 02:12:09 -0300

    Initial commit Design of multiple layouts like:    *login    *sign up    *password forgotten Design of ripples in order not to loose material design properties in buttons

---
+++ app/src/main/java/com/martin/finalapp/activities/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.martin.finalapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.martin.finalapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
